ad72f1b7f65dbfeb79d78e3f309e8ba202a502cd
Prasad <BPrasad@INTERNAL.COLT.NET>
2020-08-03 12:48:08 +0530

    product class and data.sql added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into product (name, price) values('shirt',2000);
+insert into product (name, price) values('jeans',4000);
+insert into product (name, price) values('bagpack',1200);
+insert into product (name, price) values('belt',340);
+insert into product (name, price) values('sunglasses',1000);
+insert into product (name, price) values('shoes',7000);
+insert into product (name, price) values('watch',1300);
