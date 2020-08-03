fffbea45024f436670ab59637f95b1b5aae6a832
Prasad <BPrasad@INTERNAL.COLT.NET>
2020-08-03 14:29:36 +0530

    repositories modified

--- src/main/java/com/bazinga/ecommerce/repository/CartRepository.java
+++ src/main/java/com/bazinga/ecommerce/repository/CartRepository.java
@@ -1,9 +1,11 @@
 package com.bazinga.ecommerce.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.bazinga.ecommerce.model.Cart;
 
+@Repository
 public interface CartRepository extends JpaRepository<Cart, Integer> {
 
 	
--- src/main/java/com/bazinga/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/bazinga/ecommerce/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.bazinga.ecommerce.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.bazinga.ecommerce.model.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
 }
